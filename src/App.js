7f5b782f3e48733c215586e84c9a3d5e67338b2e
RodrigoLago <rodrigazo98@gmail.com>
2022-07-29 01:47:20 -0300

    Update js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Body } from './Components/Body/Body.js';
-
+import useScript from './Components/Hooks/Hooks.js';
 function App(){
+  useScript('../src/js/Animation.js')
   return(
     <div id="App">
       <Body/>
