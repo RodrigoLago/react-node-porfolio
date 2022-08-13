42ac34161765e4034ac03d197162dc50d234bd6e
RodrigoLago <rodrigazo98@gmail.com>
2022-08-13 17:23:44 -0300

    Preload
    Add a preload clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Body } from './Components/Body/Body.js';
+import { MoonLoader } from 'react-spinners';
+import "./App.css"
+function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
 
-function App(){
-  return(
-    <div id="App">
-      <Body/>
+  return (
+    <div id="App" className="App">
+      {
+        loading ?
+          <div className="loader" >
+            <MoonLoader
+              size={60}
+              color={"#00ff9d"}
+              loading={loading} />
+          </div>
+          :
+          <Body />
+      }
+
     </div>
   )
 }
