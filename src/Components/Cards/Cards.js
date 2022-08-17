384e529a583de23a56660f6054cd51959d097f2b
RodrigoLago <rodrigazo98@gmail.com>
2022-08-17 02:42:10 -0300

    Update cards
    Adding the user api card

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -2,74 +2,99 @@ import React from 'react';
 import './Cards.css'
 export const Cards = () => {
   return <div id="proyectos-section">
-   
-       
-          <div className="portfolio-container">
+    {/*ECOMMERCE*/}
+    <div className="portfolio-container">
 
-            <div className="portfolio-card">
-              <a href="https://github.com/ProyectoEcommerce" target="_blank">
-                <div className="portfolioImage">
-                  <img
-                    src="/img/ecommerce.webp"
-                    alt="Ecommerce"
-                    srcSet=""
-                  />
+      <div className="portfolio-card">
+        <a href="https://github.com/ProyectoEcommerce" target="_blank">
+          <div className="portfolioImage">
+            <img
+              src="/img/ecommerce.webp"
+              alt="Ecommerce"
+              srcSet=""
+            />
 
-                </div>
-              </a>
-              <div className="portfolioContent">
-                <h5 className="cardCategory">Ecommerce website</h5>
-                <h3 className="portfolioTitle"><a href="https://github.com/ProyectoEcommerce" target="_blank">Top Choise Hardware</a></h3>
-                <span>
-                  <h4>
-                    Ecommerce hecho en .Net con arquitectura de microservicios
-                    y base de datos SQL Server. Front-end hecho con Html, Css y
-                    Javascript. Se trabajó con un equipo de desarrollo utilizando
-                    Scrum.</h4>
-
-                </span>
-                <div className="row fila-tags">
-                  <h5 className="col-lg-4 tags">.Net</h5>
-                  <h5 className="col-lg-4 tags">Api Rest</h5>
-                  <h5 className="col-lg-4 tags">SQL Server</h5>
-                </div>
-              </div>
-            </div>
           </div>
-        
-        
-        <div className="portfolio-container">
-          <div className="portfolio-card">
-            <a href='https://github.com/Hitchway' target="_blank">
-              <div className="portfolioImage">
-                <img
-                  src="/img/cine.webp"
-                  alt="Hitchway"
-                  srcSet=""
-                />
-              </div>
-            </a>
-            <div className="portfolioContent">
-              <h5 className="cardCategory">Website</h5>
-              <h3 className="portfolioTitle"><a href='https://github.com/Hitchway' target="_blank">Hitchway Cinema</a></h3>
-              <span><h4>
-                Website para gestionar ventas de entradas de un cine.
-                El back-end está realizado con arquitectura monolitica en .Net,
-                DB SQLServer y front-end con Html, Css y Javascript.</h4>
+        </a>
+        <div className="portfolioContent">
+          <h5 className="cardCategory">.Net 5 + Html/Css/Js</h5>
+          <h3 className="portfolioTitle"><a href="https://github.com/ProyectoEcommerce" target="_blank">Top Choise Hardware</a></h3>
+          <span>
+            <h4>
+              Ecommerce hecho en .Net con arquitectura de microservicios
+              y base de datos SQL Server. Front-end hecho con Html, Css y
+              Javascript. Se trabajó con un equipo de desarrollo utilizando
+              Scrum.</h4>
 
- 
-
-                  </span>
-              <div className="row fila-tags">
-                <h5 className="col-lg-4 tags">.Net</h5>
-                <h5 className="col-lg-4 tags">Api Rest</h5>
-                <h5 className="col-lg-4 tags">SQL Server</h5>
-              </div>
-            </div>
+          </span>
+          <div className="row fila-tags">
+            <h5 className="col-lg-4 tags">.Net 5</h5>
+            <h5 className="col-lg-4 tags">Api Rest</h5>
+            <h5 className="col-lg-4 tags">SQL Server</h5>
           </div>
         </div>
+      </div>
+    </div>
+    {/*USER API*/}
+    <div className="portfolio-container">
+      <div className="portfolio-card">
+        <a href='https://login-lagorodrigo.herokuapp.com/' target="_blank">
+          <div className="portfolioImage">
+            <img
+              src="/img/api.webp"
+              alt="UserApi"
+              srcSet=""
+            />
+          </div>
+        </a>
+        <div className="portfolioContent">
+          <h5 className="cardCategory">.Net 6 Api + React</h5>
+          <h3 className="portfolioTitle page-scroll"><a href='https://login-lagorodrigo.herokuapp.com/' target="_blank">User Api</a></h3>
+          <span>
+            <h4>Microservicio realizado en .Net 6 que da uso de MailKit, JWT, Entity Framework y encriptación, acompañado
+              de un front realizado con React.
+            </h4>
+
+          </span>
+          <div className="row fila-tags">
+            <h5 className="col-lg-6 tags">.Net 6</h5>
+            <h5 className="col-lg-6 tags">React</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/*HITCHWAY*/}
+    <div className="portfolio-container">
+      <div className="portfolio-card">
+        <a href='https://github.com/Hitchway' target="_blank">
+          <div className="portfolioImage">
+            <img
+              src="/img/cine.webp"
+              alt="Hitchway"
+              srcSet=""
+            />
+          </div>
+        </a>
+        <div className="portfolioContent">
+          <h5 className="cardCategory">.Net 5 + Html/Css/Js</h5>
+          <h3 className="portfolioTitle"><a href='https://github.com/Hitchway' target="_blank">Hitchway Cinema</a></h3>
+          <span><h4>
+            Website para gestionar ventas de entradas de un cine.
+            El back-end está realizado con arquitectura monolitica en .Net,
+            DB SQLServer y front-end con Html, Css y Javascript.</h4>
 
 
+
+          </span>
+          <div className="row fila-tags">
+            <h5 className="col-lg-4 tags">.Net 5</h5>
+            <h5 className="col-lg-4 tags">SQL Server</h5>
+            <h5 className="col-lg-4 tags">Api Rest</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    {/*PORTFOLIO*/}
     <div className="portfolio-container">
       <div className="portfolio-card">
         <a href='https://github.com/RodrigoLago/react-node-porfolio' target="_blank">
@@ -82,7 +107,7 @@ export const Cards = () => {
           </div>
         </a>
         <div className="portfolioContent">
-          <h5 className="cardCategory">Website</h5>
+          <h5 className="cardCategory">React + Node.js</h5>
           <h3 className="portfolioTitle page-scroll"><a href='https://github.com/RodrigoLago/react-node-porfolio' target="_blank">Portfolio</a></h3>
           <span>
             <h4>El sitio web que usted está viendo en este momento
@@ -97,5 +122,6 @@ export const Cards = () => {
         </div>
       </div>
     </div>
+
   </div>
 }
